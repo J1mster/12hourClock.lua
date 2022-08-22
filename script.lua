e03381538b63c845cd7bec342b3a5623d7633aeb
Jim <82846092+JimCantFindAUsername@users.noreply.github.com>
2022-08-22 17:23:29 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
---	          JJJJJJJJJJJ  iiii                                                    tttt                                                  	 
+--		 	  JJJJJJJJJJJ  iiii                                                    tttt                                                  	 
 --	          J:::::::::J i::::i                                                ttt:::t                                                  	 
 --	          J:::::::::J  iiii                                                 t:::::t                                                  	 
 --	          JJ:::::::JJ                                                       t:::::t                                                  	 
@@ -11,20 +11,34 @@
 --	J:::::J     J:::::J   i::::i m::::m   m::::m   m::::m      s::::::s         t:::::t          e::::::eeeeeeeeeee   r:::::r            	 
 --	J::::::J   J::::::J   i::::i m::::m   m::::m   m::::mssssss   s:::::s       t:::::t    tttttte:::::::e            r:::::r            	 
 --	J:::::::JJJ:::::::J  i::::::im::::m   m::::m   m::::ms:::::ssss::::::s      t::::::tttt:::::te::::::::e           r:::::r            	 
---       JJ:::::::::::::JJ   i::::::im::::m   m::::m   m::::ms::::::::::::::s       tt::::::::::::::t e::::::::eeeeeeee   r:::::r            	 
+--   JJ:::::::::::::JJ   i::::::im::::m   m::::m   m::::ms::::::::::::::s       tt::::::::::::::t e::::::::eeeeeeee   r:::::r            	 
 --	   JJ:::::::::JJ     i::::::im::::m   m::::m   m::::m s:::::::::::ss          tt:::::::::::tt  ee:::::::::::::e   r:::::r            	 
 --	     JJJJJJJJJ       iiiiiiiimmmmmm   mmmmmm   mmmmmm  sssssssssss              ttttttttttt      eeeeeeeeeeeeee   rrrrrrr      
 
 local TimeUI = script.Parent
 
 local irlHour = os.date("t")["hour"]
+local irlMin = os.date("t")["Hour"]
 
 while true do
-	if irlHour == > 12 then
-		TimeUI.Text = os.date("*t")["hour"]-12 ..":" .. os.date("*t")["min"] .."PM"
-	elseif irlHour == < 12
-		TimeUI.Text = os.date("*t")["hour"] ..":" .. os.date("*t")["min"]
+	if irlHour > 12 then
+		if irlMin < 10 then
+			
+			TimeUI.Text = os.date("*t")["hour"]-12 ..":0" .. os.date("*t")["min"] .."PM"
+			
+		elseif irlMin > 10 
+			
+			TimeUI.Text = os.date("*t")["hour"]-12 ..":" .. os.date("*t")["min"] .."PM"
+			
+		end
+	else
+		if irlMin  < 10 then
+			TimeUI.Text = os.date("*t")["hour"] ..":0" .. os.date("*t")["min"]
+		elseif irlMin > 10
+			TimeUI.Text = os.date("*t")["hour"] ..":" .. os.date("*t")["min"]
+		end
 	end
+
 
 	wait(5)
 end
