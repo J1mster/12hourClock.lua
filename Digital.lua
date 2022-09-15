d40465116391de362c87d1ce9090344806b67344
Jim <82846092+JimCantFindAUsername@users.noreply.github.com>
2022-09-15 11:55:20 -0500

    Update Digital.lua

--- Digital.lua
+++ Digital.lua
@@ -15,24 +15,51 @@
 --	   JJ:::::::::JJ     i::::::im::::m   m::::m   m::::m s:::::::::::ss          tt:::::::::::tt  ee:::::::::::::e   r:::::r            	 
 --	     JJJJJJJJJ       iiiiiiiimmmmmm   mmmmmm   mmmmmm  sssssssssss              ttttttttttt      eeeeeeeeeeeeee   rrrrrrr      
 
+
+local SecondsEnabled == false
+
+
+
 local TimeUI = script.Parent
 
 while true do 
-	if os.date("*t").hour > 12 then 
+	if SecondsEnabled == false then
+		if os.date("*t").hour > 12 then 
 		wait(1)
-		if os.date("*t").min > 10 then
-			TimeUI.Text = os.date("*t").hour-12 ..":" .. os.date("*t").min .."PM" 
-		else
-			TimeUI.Text = os.date("*t").hour-12 ..":0" .. os.date("*t").min .."PM" 
+			if os.date("*t").min > 10 then
+				TimeUI.Text = os.date("*t").hour-12 ..":" .. os.date("*t").min .."AM" 
+			else
+				TimeUI.Text = os.date("*t").hour-12 ..":0" .. os.date("*t").min .."AM" 
+			end
+		else 
+			if os.date("*t").min > 10 then
+				TimeUI.Text = os.date("*t").min ..":" .. os.date("*t").min .. "PM"
+			else
+				TimeUI.Text = os.date("*t").min ..":0" .. os.date("*t").min .. "PM"
+			end
 		end
-	else 
-		wait(1)
-		if os.date("*t").min > 10 then
-			TimeUI.Text = os.date("*t").min ..":" .. os.date("*t").min 
-		else
-			TimeUI.Text = os.date("*t").min ..":0" .. os.date("*t").min 
+	elseif SecondsEnabled == true then
+		if os.date("*t").hour > 12 then 
+			if os.date("*t").min > 10 then
+				TimeUI.Text = os.date("*t").hour-12 ..":" .. os.date("*t").min ..":" .. os.date("*t").sec .."AM" 
+			else
+				TimeUI.Text = os.date("*t").hour-12 ..":0" .. os.date("*t").min ..":" .. os.date("*t").sec .."AM" 
+			end
+		else 
+			if os.date("*t").min > 10 then
+				if  os.date("*t").sec>10 then
+					TimeUI.Text = os.date("*t").min ..":" .. os.date("*t").min .. ":0" .. os.date("*t").sec .. "PM"
+				else
+					TimeUI.Text = os.date("*t").min ..":" .. os.date("*t").min .. ":" .. os.date("*t").sec .. "PM"
+				end
+			else
+				if  os.date("*t").sec>10 then
+					TimeUI.Text = os.date("*t").min ..":0" .. os.date("*t").min .. ":0" .. os.date("*t").sec .. "PM"
+				else
+					TimeUI.Text = os.date("*t").min ..":0" .. os.date("*t").min .. ":" .. os.date("*t").sec .. "PM"
+				end
+			end
 		end
 	end
-
-	wait(5)
+	wait(0.1)
 end
